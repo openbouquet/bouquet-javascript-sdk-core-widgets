e7c923fdf128ed8db25db94792537de7c8ea7ff0
olivier <obalbous@squidsolutions.com>
2016-01-22 18:34:50 +0100

    T685 keep templates in templates.js and do not update /dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,48 +7,18 @@ module.exports = function(grunt) {
                 clean : {
                     all : "dist/"
                 },
-                bower_concat : {
-                    all : {
-                        dest : 'build/_bower.js',
-                        exclude : [ 'squid_api', "jquery",
-                                    "underscore",
-                                    "backbone"],
-                        bowerOptions : {
-                            relative : false
-                        }
-                    }
-                },
                 concat : {
                     options : {
                         stripBanners : true,
                     },
-                        js : {
-                            src : [ 'build/templates.js',
-                                    'src/*.js' ],
-                            dest : 'dist/squid_api_core-widgets.js',
-                        },
-                        css : {
-                            src : [ 'src/*.css' ],
-                            dest : 'dist/squid_api_core-widgets.css',
-                        },
-                    dev : {
-                        files : {
-                             'dist/squid_api_login.js': ['dist/squid_api_login_template.js','src/squid_api_login.js'],
-                             'dist/squid_api_pagination.js': ['dist/squid_api_pagination_template.js','src/squid_api_pagination.js'],
-                             'dist/squid_api_selector.js': ['dist/squid_api_selector_template.js','src/squid_api_selector.js'],
-                             'dist/squid_api_status.js': ['dist/squid_api_status_template.js','src/squid_api_status.js'],
-                             'dist/squid_api_switch.js': ['dist/squid_api_switch_template.js','src/squid_api_switch.js']
-                        }
-                    }
-                },
-                copy: {
-                    devDist: {
-                        files: [{
-                            expand: true,
-                            flatten: true,
-                            src : [ 'src/*.js' ],
-                            dest: 'dist'
-                        }]
+                    js : {
+                        src : [ 'build/templates.js',
+                                'src/*.js' ],
+                        dest : 'dist/squid_api_core-widgets.js',
+                    },
+                    css : {
+                        src : [ 'src/*.css' ],
+                        dest : 'dist/squid_api_core-widgets.css',
                     }
                 },
                 handlebars : {
@@ -63,15 +33,6 @@ module.exports = function(grunt) {
                         files : {
                             "build/templates.js" : [ "src/*.hbs" ]
                         }
-                    },
-                    dev:{
-                        files : {
-                            'dist/squid_api_login_template.js': ['src/squid_api_login.hbs'],
-                            'dist/squid_api_pagination_template.js': ['src/squid_api_pagination.hbs'],
-                            'dist/squid_api_selector_template.js': ['src/squid_api_selector.hbs'],
-                            'dist/squid_api_status_template.js': ['src/squid_api_status.hbs'],
-                            'dist/squid_api_switch_template.js': ['src/squid_api_switch.hbs']
-                        }
                     }
                 },
                 watch : {
@@ -85,11 +46,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-handlebars');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-bower-concat');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('devDist', [ 'jshint', 'clean', 'handlebars:dev',  'copy:devDist', 'concat:dev' ]);
+    grunt.registerTask('dev', [ 'jshint', 'handlebars']);
 
     grunt.registerTask('default', [ 'jshint', 'clean', 'handlebars:all', 'concat:js', 'concat:css']);
 };
